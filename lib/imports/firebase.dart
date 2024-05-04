197a8ca92619ba361c0ff6b51010b08406357c65
mohamadlounnas <mohamadlounnas@gmail.com>
2024-05-04 16:39:48 +0100

    Update Firebase dependencies and remove unused imports

--- lib/imports/firebase.dart
+++ lib/imports/firebase.dart
@@ -1,15 +1,11 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:cloud_firestore/cloud_firestore.dart';
 export 'package:cloud_functions/cloud_functions.dart';
 export 'package:firebase_analytics/firebase_analytics.dart';
-export 'package:firebase_app_check/firebase_app_check.dart';
+// export 'package:firebase_app_check/firebase_app_check.dart';
 export 'package:firebase_auth/firebase_auth.dart';
 export 'package:firebase_core/firebase_core.dart';
 export 'package:firebase_crashlytics/firebase_crashlytics.dart';
-
-
-
 
 /// add extension to [fb.FirebaseFirestore] to use cache
 extension FirestoreDocumentExtension<T> on DocumentReference<T> {
